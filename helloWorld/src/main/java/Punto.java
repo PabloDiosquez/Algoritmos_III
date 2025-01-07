be1b71809a518791a00fa4781e258b3603b664fc
Pablo Diosquez <pablodiosquez24@gmail.com>
2025-01-07 08:28:36 -0300

    Se agrega 'Punto'

--- helloWorld/src/main/java/Punto.java
+++ helloWorld/src/main/java/Punto.java
@@ -1,6 +1,6 @@
 public class Punto {
-    int x;
-    int y;
+    private int x;
+    private int y;
     public Punto(){
         this.x = 0;
         this.y = 0;
@@ -12,5 +12,26 @@ public class Punto {
     public Punto(Punto otro){
         this.x = otro.x;
         this.y = otro.y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(x: %d, y: %d)", this.x, this.y);
     }
 }
