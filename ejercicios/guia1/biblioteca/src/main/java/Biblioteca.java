68baad45126c7baa1db9432002d5909e570c1509
Pablo Diosquez <pablodiosquez24@gmail.com>
2025-01-10 10:14:47 -0300

    Agregar los métodos agregarLibro, quitarLibro, buscar, en la clase Biblioteca

--- ejercicios/guia1/biblioteca/src/main/java/Biblioteca.java
+++ ejercicios/guia1/biblioteca/src/main/java/Biblioteca.java
@@ -5,6 +5,7 @@ import lombok.NonNull;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import java.util.function.Predicate;
 
 @Builder
@@ -21,7 +22,41 @@ public class Biblioteca {
 
     // -------------------- LIBROS --------------------
 
-    
+    public void agregarLibro(String titulo){
+        if(pertenece(titulo)){
+            buscar(titulo).actualizarStock(1);
+            return;
+        }
+        Libro libro = Libro.builder()
+                .ISBN(generarISBN())
+                .titulo(titulo)
+                .build();
+        libros.add(libro);
+    }
+
+    public String quitarLibro(String titulo){
+        if(pertenece(titulo)){
+            Libro libro = buscar(titulo);
+            libros.remove(libro);
+            return libro.getTitulo();
+        }
+        return "El libro no forma parte de la colección de la biblioteca";
+    }
+
+    private Libro buscar(String titulo){
+        return buscar(libro -> libro.getTitulo().equals(titulo));
+    }
+    private Libro buscar(Predicate<Libro> criterio){
+        Iterator<Libro> iter = libros.iterator();
+        while(iter.hasNext()){
+            Libro libro = iter.next();
+            if(criterio.test(libro)){
+                return libro;
+            }
+        }
+        return null;
+    }
+
     private boolean pertenece(int ISBN){
         return pertenece(libro -> libro.getISBN() == ISBN);
     }
@@ -44,16 +79,21 @@ public class Biblioteca {
     // -------------------- CLIENTES --------------------
 
 
+    private boolean esCliente(String nombre, String apellido){
+        Iterator<Cliente> iter = clientes.iterator();
+        while(iter.hasNext()){
+            Cliente cliente = iter.next();
+            if(cliente.getNombre().equals(nombre) && cliente.getApellido().equals(apellido)){
+                return true;
+            }
+        }
+        return false;
+    }
 
+    // --------------------------------------------------
 
-
-
-
-
-
-
-
-
-
+    private int generarISBN(){
+        return new Random().nextInt(1000);
+    }
 
 }
