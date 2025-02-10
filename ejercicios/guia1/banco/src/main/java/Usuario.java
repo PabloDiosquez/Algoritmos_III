2c1a8fe405ab81fef877f3f82f471a630505d2ba
Pablo Diosquez <pablodiosquez24@gmail.com>
2025-02-10 15:54:59 -0300

    Agregar atributos de la clase Usuario

--- ejercicios/guia1/banco/src/main/java/Usuario.java
+++ ejercicios/guia1/banco/src/main/java/Usuario.java
@@ -1,2 +1,14 @@
+import lombok.Builder;
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+@Builder
 public class Usuario {
+    @NonNull
+    private String id;
+    @NonNull
+    private String nombre;
+    @Builder.Default
+    private List<Cuenta> cuentas = new ArrayList<>();
 }
