461247c8e36a7d874c83e5fb654a1f4700bdc060
Pablo Diosquez <pablodiosquez24@gmail.com>
2025-02-10 16:07:49 -0300

    Agregar métodos de la clase Cuenta y CajaDeAhorro

--- ejercicios/guia1/banco/src/main/java/CajaDeAhorro.java
+++ ejercicios/guia1/banco/src/main/java/CajaDeAhorro.java
@@ -1,2 +1,11 @@
-public class CajaDeAhorro {
+public class CajaDeAhorro extends Cuenta{
+    private double tasaDeInteres;
+
+    public double calcularInteres(int meses){
+        return obtenerSaldo()*(1 + tasaDeInteres * meses);
+    }
+
+    public void aplicarInteres(int meses){
+        saldo = obtenerSaldo()*(1 + tasaDeInteres * meses);
+    }
 }
--- ejercicios/guia1/banco/src/main/java/Cuenta.java
+++ ejercicios/guia1/banco/src/main/java/Cuenta.java
@@ -1,2 +1,25 @@
 public class Cuenta {
+    private String id;
+    private String titular;
+    protected double saldo;
+
+    public boolean depositar(double monto){
+        if(monto < 0){
+            return false;
+        }
+        saldo += monto;
+        return true;
+    }
+
+    public boolean retirar(double monto){
+        if(saldo - monto <= 0){
+            return false;
+        }
+        saldo -= monto;
+        return true;
+    }
+
+    public double obtenerSaldo(){
+        return saldo;
+    }
 }
