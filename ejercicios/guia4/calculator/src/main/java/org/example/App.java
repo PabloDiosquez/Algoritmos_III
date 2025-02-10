c5f641811a9c18b20f49469a355f156e559541c6
Pablo Diosquez <pablodiosquez24@gmail.com>
2025-02-10 14:48:34 -0300

    Actualizar calculator app

--- ejercicios/guia4/calculator/src/main/java/org/example/App.java
+++ ejercicios/guia4/calculator/src/main/java/org/example/App.java
@@ -1,11 +1,16 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.TilePane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+
 
 
 /**
@@ -15,13 +20,61 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        TilePane pane = new TilePane();
+        var root = new VBox();
 
-        Scene scene = new Scene(pane, 200, 200);
+        var screen = new TextField();
+        var panel = new VBox();
+        panel.setId("panelId");
+
+        root.getChildren().add(screen);
+        root.getChildren().add(panel);
+
+        var r1 = insertRowInto(panel);
+        insertButtonInto(r1, "7");
+        insertButtonInto(r1, "8");
+        insertButtonInto(r1, "9");
+        insertButtonInto(r1, "/");
+
+        var r2 = insertRowInto(panel);
+        insertButtonInto(r2, "4");
+        insertButtonInto(r2, "5");
+        insertButtonInto(r2, "6");
+        insertButtonInto(r2, "*");
+
+        var r3 = insertRowInto(panel);
+        insertButtonInto(r3, "1");
+        insertButtonInto(r3, "2");
+        insertButtonInto(r3, "3");
+        insertButtonInto(r3, "+");
+
+        var r4 = insertRowInto(panel);
+        insertButtonInto(r4, "C");
+        insertButtonInto(r4, "0");
+        insertButtonInto(r4, "=");
+        insertButtonInto(r4, "-");
+
+        Scene scene = new Scene(root, 200, 200);
         stage.setScene(scene);
         stage.show();
     }
     public static void main(String[] args) {
         launch();
+    }
+
+    private HBox insertRowInto(VBox panel){
+        var row = new HBox();
+
+        panel.getChildren().add(row);
+        return row;
+    }
+
+    private Button insertButtonInto(HBox row, String content) {
+        var btn = new Button(content);
+        row.getChildren().add(btn);
+        btn.setOnAction(ActionEvent -> {
+            System.out.println();
+        });
+
+        return btn;
     }
 }
