e09b56e51a507f441cb93bd9189d7b3cbebc5a25
Pablo Diosquez <pablodiosquez24@gmail.com>
2025-02-09 22:02:06 -0300

    Actualizar choiceBox app

--- ejercicios/guia4/choiceBox/src/main/java/org/example/App.java
+++ ejercicios/guia4/choiceBox/src/main/java/org/example/App.java
@@ -1,14 +1,11 @@
 package org.example;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
