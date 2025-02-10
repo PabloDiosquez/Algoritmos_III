e6c4d1982c3c17666406d963a4594fb89eb52799
Pablo Diosquez <pablodiosquez24@gmail.com>
2025-02-10 15:26:45 -0300

    Actualizar calculator app

--- ejercicios/guia4/calculator/src/main/java/org/example/App.java
+++ ejercicios/guia4/calculator/src/main/java/org/example/App.java
@@ -17,46 +17,53 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
+    private VBox root;
+    private TextField supraScreen;
+    private TextField screen;
+    private VBox panel;
 
     @Override
     public void start(Stage stage) throws Exception {
-        var root = new VBox();
+        root = new VBox();
 
-        var screen = new TextField();
-        var panel = new VBox();
-        panel.setId("panelId");
+        supraScreen = new TextField();
+        root.getChildren().add(supraScreen);
 
+        screen = new TextField();
         root.getChildren().add(screen);
+
+        panel = new VBox();
         root.getChildren().add(panel);
 
         var r1 = insertRowInto(panel);
         insertButtonInto(r1, "7");
         insertButtonInto(r1, "8");
         insertButtonInto(r1, "9");
-        insertButtonInto(r1, "/");
+        insertOpButtonInto(r1, "/");
 
         var r2 = insertRowInto(panel);
         insertButtonInto(r2, "4");
         insertButtonInto(r2, "5");
         insertButtonInto(r2, "6");
-        insertButtonInto(r2, "*");
+        insertOpButtonInto(r2, "*");
 
         var r3 = insertRowInto(panel);
         insertButtonInto(r3, "1");
         insertButtonInto(r3, "2");
         insertButtonInto(r3, "3");
-        insertButtonInto(r3, "+");
+        insertOpButtonInto(r3, "+");
 
         var r4 = insertRowInto(panel);
-        insertButtonInto(r4, "C");
+        insertClearButtonInto(r4, "C");
         insertButtonInto(r4, "0");
-        insertButtonInto(r4, "=");
-        insertButtonInto(r4, "-");
+        insertEqualButtonInto(r4, "=");
+        insertOpButtonInto(r4, "-");
 
         Scene scene = new Scene(root, 200, 200);
         stage.setScene(scene);
         stage.show();
     }
+
     public static void main(String[] args) {
         launch();
     }
@@ -68,13 +75,41 @@ public class App extends Application {
         return row;
     }
 
-    private Button insertButtonInto(HBox row, String content) {
+    private void insertButtonInto(HBox row, String content) {
         var btn = new Button(content);
         row.getChildren().add(btn);
-        btn.setOnAction(ActionEvent -> {
-            System.out.println();
+        btn.setOnAction(event -> {
+            String supraText = supraScreen.getText() + ((Button)(event.getSource())).getText();
+            String newText = screen.getText() + ((Button)(event.getSource())).getText();
+            supraScreen.setText(supraText);
+            screen.setText(newText);
         });
+    }
 
-        return btn;
+    private void insertEqualButtonInto(HBox row, String content){
+        var btn = new Button(content);
+        row.getChildren().add(btn);
+        btn.setOnAction(event -> {
+            System.out.println("Hi! I'm = :)");
+        });
+    }
+
+    private void insertClearButtonInto(HBox row, String content) {
+        var btn = new Button(content);
+        row.getChildren().add(btn);
+        btn.setOnAction(actionEvent -> {
+            supraScreen.clear();
+            screen.clear();
+        });
+    }
+
+    private void insertOpButtonInto(HBox row, String content) {
+        var btn = new Button(content);
+        row.getChildren().add(btn);
+        btn.setOnAction(event -> {
+            String newText = screen.getText() + ((Button)(event.getSource())).getText();
+            supraScreen.setText(newText);
+            screen.clear();
+        });
     }
 }
---
+++ ejercicios/guia4/calculator/src/main/java/org/example/Model.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Model {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static int prod(int a, int b){
+        return a*b;
+    }
+    public static int div(int a, int b){
+        return a/b;
+    }
+    public static int sub(int a, int b){
+        return a-b;
+    }
+}
